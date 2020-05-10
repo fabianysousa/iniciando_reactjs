0624241d8d39b09bbd6a378d558dd3e92893ccc4
Fabiany de Sousa <37695655+fabianysousa@users.noreply.github.com>
2020-05-10 18:43:07 -0300

    commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Post from './Post';
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Hello World</h1>
+                <Post title="Aprendendo ReactJS" />
+                <Post title="Aprendendo ReactJS" />
+                <Post title="Aprendendo ReactJS" />
+                <Post title="Aprendendo ReactJS" />
+            </div>
+        );
+    }
+}
---
+++ src/components/Post.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import Coment from './Coment';
+
+export default class Post extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            coments: [],
+
+            newCommentText: ''
+        };
+
+       
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+///////////////////////////////////////////////////
+    handleSubmit(e){
+        this.setState({
+            
+            coments: [
+                ...this.state.coments,
+                { text: this.state.newCommentText }
+            ]
+
+        });
+
+        this.setState({ newCommentText: '' });
+
+        e.preventDefault();
+    }
+//////////////////////////////////////////////////////
+    handleTextChange(e){
+        this.setState({ newCommentText: e.target.value })
+    }
+
+
+
+
+
+
+    render() {
+        return(
+            <div>
+                <h2>{ this.props.title }</h2>
+
+                <form onSubmit={ this.handleSubmit}>
+
+                    <input 
+                        value={this.state.newCommentText} 
+                        onChange={this.handleTextChange} 
+                    />
+                    <button type="submit">Comentar</button>
+                
+                </form>
+
+                { this.state.coments.map((coment, index) => {
+                    return <Coment key={index} text={coment.text} />
+                }) }
+                 
+            </div>
+        );
+    }    
+}
